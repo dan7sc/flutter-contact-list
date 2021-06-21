519111d0110c0fef721935131b294b5a4b68494e
dan7sc <su7cheng@gmail.com>
2021-06-21 19:38:37 -0300

    Add padding inside row

--- lib/main.dart
+++ lib/main.dart
@@ -65,10 +65,12 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                child: Stack(
-                  children: [
-                    Container(
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Container(
+                  child: Stack(
+                    children: [
+                      Container(
                         width: 60.0,
                         height: 60.0,
                         margin: EdgeInsets.all(4.0),
@@ -81,46 +83,47 @@ class _MyHomePageState extends State<MyHomePage> {
                           color: Colors.grey.shade300,
                         ),
                       ),
-                    Positioned(
-                      bottom: 9.0,
-                      right: 9.0,
-                      child: Container(
-                        width: 12.0,
-                        height: 12.0,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                        child: Center(
-                          child: Container(
-                            width: 10.0,
-                            height: 10.0,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.green.shade300,
+                      Positioned(
+                        bottom: 9.0,
+                        right: 9.0,
+                        child: Container(
+                          width: 12.0,
+                          height: 12.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                          child: Center(
+                            child: Container(
+                              width: 10.0,
+                              height: 10.0,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.green.shade300,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 0.0,
-                      left: 0.0,
-                      child: Container(
-                        width: 20.0,
-                        height: 20.0,
-                        child: Icon(
-                          Icons.star,
-                          size: 15.0,
-                          color: Colors.white,
-                        ),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.yellow.shade700,
+                      Positioned(
+                        top: 0.0,
+                        left: 0.0,
+                        child: Container(
+                          width: 20.0,
+                          height: 20.0,
+                          child: Icon(
+                            Icons.star,
+                            size: 15.0,
+                            color: Colors.white,
+                          ),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.yellow.shade700,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
