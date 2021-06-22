13d4b61a9f0c1c87a68514a33dc2db2d131f2ba7
dan7sc <su7cheng@gmail.com>
2021-06-22 11:09:56 -0300

    Add date and number of messages

--- lib/main.dart
+++ lib/main.dart
@@ -66,104 +66,142 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Row(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: 60.0,
-                          height: 60.0,
-                          margin: EdgeInsets.all(4.0),
-                          child: Image.network(
-                            "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
-                            fit: BoxFit.cover,
-                          ),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 9.0,
-                          right: 9.0,
-                          child: Container(
-                            width: 12.0,
-                            height: 12.0,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: 60.0,
+                              height: 60.0,
+                              margin: EdgeInsets.all(4.0),
+                              child: Image.network(
+                                "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
+                                fit: BoxFit.cover,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey.shade300,
+                              ),
                             ),
-                            child: Center(
+                            Positioned(
+                              bottom: 9.0,
+                              right: 9.0,
                               child: Container(
-                                width: 10.0,
-                                height: 10.0,
+                                width: 12.0,
+                                height: 12.0,
                                 decoration: BoxDecoration(
                                   shape: BoxShape.circle,
-                                  color: Colors.green.shade300,
+                                  color: Colors.white,
+                                ),
+                                child: Center(
+                                  child: Container(
+                                    width: 10.0,
+                                    height: 10.0,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.green.shade300,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                        Positioned(
-                          top: 0.0,
-                          left: 0.0,
-                          child: Container(
-                            width: 20.0,
-                            height: 20.0,
-                            child: Icon(
-                              Icons.star,
-                              size: 15.0,
-                              color: Colors.white,
-                            ),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.yellow.shade700,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text(
-                        "Jennifer Fritz",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Container(
-                        width: 250,
-                        child: Flex(
-                          direction: Axis.vertical,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Flexible(
-                              child: Text(
-                                "Jon it differed repeated wandered required in. Then girl neat why. Jon it differed repeated wandered required in. Then girl neat why",
-                                softWrap: true,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
+                            Positioned(
+                              top: 0.0,
+                              left: 0.0,
+                              child: Container(
+                                width: 20.0,
+                                height: 20.0,
+                                child: Icon(
+                                  Icons.star,
+                                  size: 15.0,
+                                  color: Colors.white,
+                                ),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.yellow.shade700,
+                                ),
                               ),
                             ),
                           ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            "Jennifer Fritz",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Container(
+                            width: 250,
+                            child: Flex(
+                              direction: Axis.vertical,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Flexible(
+                                  child: Text(
+                                    "Jon it differed repeated wandered required in. Then girl neat why. Jon it differed repeated wandered required in. Then girl neat why",
+                                    softWrap: true,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Text(
+                                "05/06",
+                              ),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 16.0),
+                                child: Center(
+                                  child: Text(
+                                    "3",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                width: 24.0,
+                                height: 24.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
