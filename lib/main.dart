1c76e69c5870320ef347032da03a7b84e5bea04c
dan7sc <su7cheng@gmail.com>
2021-06-21 22:06:22 -0300

    Contact info inside a card

--- lib/main.dart
+++ lib/main.dart
@@ -60,110 +60,113 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  child: Stack(
-                    children: [
-                      Container(
-                        width: 60.0,
-                        height: 60.0,
-                        margin: EdgeInsets.all(4.0),
-                        child: Image.network(
-                          "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
-                          fit: BoxFit.cover,
-                        ),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.grey.shade300,
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 9.0,
-                        right: 9.0,
-                        child: Container(
-                          width: 12.0,
-                          height: 12.0,
+          Card(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 60.0,
+                          height: 60.0,
+                          margin: EdgeInsets.all(4.0),
+                          child: Image.network(
+                            "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
+                            fit: BoxFit.cover,
+                          ),
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
-                            color: Colors.white,
+                            color: Colors.grey.shade300,
                           ),
-                          child: Center(
-                            child: Container(
-                              width: 10.0,
-                              height: 10.0,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.green.shade300,
+                        ),
+                        Positioned(
+                          bottom: 9.0,
+                          right: 9.0,
+                          child: Container(
+                            width: 12.0,
+                            height: 12.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: Center(
+                              child: Container(
+                                width: 10.0,
+                                height: 10.0,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.green.shade300,
+                                ),
                               ),
                             ),
                           ),
                         ),
+                        Positioned(
+                          top: 0.0,
+                          left: 0.0,
+                          child: Container(
+                            width: 20.0,
+                            height: 20.0,
+                            child: Icon(
+                              Icons.star,
+                              size: 15.0,
+                              color: Colors.white,
+                            ),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.yellow.shade700,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "Jennifer Fritz",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                      Positioned(
-                        top: 0.0,
-                        left: 0.0,
-                        child: Container(
-                          width: 20.0,
-                          height: 20.0,
-                          child: Icon(
-                            Icons.star,
-                            size: 15.0,
-                            color: Colors.white,
-                          ),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.yellow.shade700,
-                          ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Container(
+                        width: 250,
+                        child: Flex(
+                          direction: Axis.vertical,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                "Jon it differed repeated wandered required in. Then girl neat why. Jon it differed repeated wandered required in. Then girl neat why",
+                                softWrap: true,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(
-                      "Jennifer Fritz",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight. w500,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Container(
-                      width: 250,
-                      child: Flex(
-                        direction: Axis.vertical,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Flexible(
-                            child: Text(
-                              "Jon it differed repeated wandered required in. Then girl neat why. Jon it differed repeated wandered required in. Then girl neat why",
-                              softWrap: true,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
