f742d09c15ae4a6bf084feeecfd74ec70dcf75b9
dan7sc <noreply@noreply.com>
2021-08-28 15:43:08 -0300

    Add ListView to body

--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -45,15 +45,11 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: false,
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          ContactCard(
-            chatData: controller.lastChats[0],
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: controller.lastChats.length,
+        itemBuilder: (context, index) => ContactCard(
+          chatData: controller.lastChats[index],
+        ),
       ),
     );
   }
